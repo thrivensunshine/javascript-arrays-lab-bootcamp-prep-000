6e137771fceb27499a88ac3cf4ec5595aae57fe9
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-01 17:29:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  
+}
+
+function destructivelyPrependKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  
+}
+
+function appendKitten(name){
+  
+}
+
+function prependKitten(name){
+  
+}
+
+function removeLastKitten(){
+  
+}
+
+function removeFirstKitten(){
+  
+}
